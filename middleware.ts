07e6e9f2f552742002b7deacde6f1fa749f9e95f
Festus <matkowsky808@gmail.com>
2024-01-14 00:04:39 +0100

    chore: Clean up middleware

--- middleware.ts
+++ middleware.ts
@@ -7,28 +7,15 @@ import type { Database } from '@/types/supabase'
 export async function middleware(req: NextRequest) {
 	const res = NextResponse.next()
 
-	// Create a Supabase client configured to use cookies
 	const supabase = createMiddlewareClient<Database>({ req, res })
 
-	// Refresh session if expired - required for Server Components
 	const {
-		data: { session }
-	} = await supabase.auth.getSession()
-
-	// if (session && )
+		data: { user }
+	} = await supabase.auth.getUser()
 
 	return res
 }
 
-// Ensure the middleware is only called for relevant paths.
 export const config = {
-	matcher: [
-		/*
-		 * Match all request paths except for the ones starting with:
-		 * - _next/static (static files)
-		 * - _next/image (image optimization files)
-		 * - favicon.ico (favicon file)
-		 */
-		'/((?!_next/static|_next/image|favicon.ico).*)'
-	]
+	matcher: []
 }
